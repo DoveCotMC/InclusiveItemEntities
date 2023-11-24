deb7d7320d471f714f84c1df5507b62f3cc06ec0
MikhailTapio <mikhailtapio13468@163.com>
2023-11-24 12:21:07 +0800

    Fix visual glitch && expand merging range for ies whose stack's count >= 64

--- src/main/java/team/dovecotmc/inclusiveie/mixin/MixinItemEntity.java
+++ src/main/java/team/dovecotmc/inclusiveie/mixin/MixinItemEntity.java
@@ -1,15 +1,44 @@
 package team.dovecotmc.inclusiveie.mixin;
 
+import net.minecraft.stats.Stats;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
+import java.util.UUID;
 
 @Mixin(ItemEntity.class)
-public abstract class MixinItemEntity {
+public abstract class MixinItemEntity extends Entity {
+    @Shadow
+    private int pickupDelay;
+
+    @Shadow
+    public abstract ItemStack getItem();
+
+    @Shadow
+    @Nullable
+    private UUID target;
+
+    public MixinItemEntity(EntityType<?> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    @Redirect(method = "mergeWithNeighbours", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/AABB;inflate(DDD)Lnet/minecraft/world/phys/AABB;"))
+    private AABB redirect$mergeWithNeighbours(AABB instance, double pX, double pY, double pZ) {
+        final double horizontal = getItem().getCount() >= 64 ? 1.5 : .5;
+        return instance.inflate(horizontal, .0, horizontal);
+    }
+
     @Redirect(method = "isMergable", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getMaxStackSize()I"))
     private int redirect$isMergable(ItemStack instance) {
         return Integer.MAX_VALUE;
@@ -31,5 +60,34 @@ public abstract class MixinItemEntity {
             constant = @Constant(intValue = 64))
     private static int redirect$merge$1(int constant) {
         return Integer.MAX_VALUE;
+    }
+
+    @Inject(method = "playerTouch", at = @At("HEAD"), cancellable = true)
+    private void inject$playerTouch(Player pEntity, CallbackInfo ci) {
+        ci.cancel();
+        if (!this.level().isClientSide) {
+            if (this.pickupDelay > 0) return;
+            ItemStack itemstack = this.getItem();
+            Item item = itemstack.getItem();
+            int i = itemstack.getCount();
+            int hook = net.minecraftforge.event.ForgeEventFactory.onItemPickup((ItemEntity) (Object) this, pEntity);
+            if (hook < 0) return;
+            ItemStack copy = itemstack.copy();
+            boolean shouldContinue = i <= 0 || hook == 1;
+            if (this.pickupDelay == 0 && (this.target == null || this.target.equals(pEntity.getUUID()))) {
+                pEntity.getInventory().add(itemstack);
+                i = copy.getCount() - itemstack.getCount();
+                if (!shouldContinue && i == 0) return;
+                copy.setCount(i);
+                net.minecraftforge.event.ForgeEventFactory.firePlayerItemPickupEvent(pEntity, (ItemEntity) (Object) this, copy);
+                pEntity.take(this, i);
+                if (itemstack.isEmpty()) {
+                    this.discard();
+                    itemstack.setCount(i);
+                }
+                pEntity.awardStat(Stats.ITEM_PICKED_UP.get(item), i);
+                pEntity.onItemPickup((ItemEntity) (Object) this);
+            }
+        }
     }
 }
